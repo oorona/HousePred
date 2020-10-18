f3c42dcc3769f851163afb3aff4d249c7a39135e
Oswaldo Orona <oorona@gmail.com>
2020-10-17 19:31:40 -0600

    Acceptable

--- mymain.R
+++ mymain.R
@@ -2,18 +2,8 @@ set.seed(2685)
 library(xgboost)
 
 
-train <- read.csv("train.csv")
-test <- read.csv("test.csv")
-
-train.x =train[-1]
-train.x =train.x[-82]
-
-train.y=log(train[,"Sale_Price"])
-
-test.x=test[-1]
-
-total=rbind(train.x,test.x)
-
+##G
+# function definitions
 getCategoricalVar = function(df){
   cat.var <- colnames(df)[
     which(sapply(df, function(x) mode(x)=="character"))]
@@ -41,48 +31,71 @@ createExpandedMatrix = function(df){
   expandedM
 }
 
-expandMatrix = function (sourceMatrix,colnames){
-  sourcenames=colnames(sourceMatrix)
+adjustMatrix = function (sourceMatrix,colnames){
+  
+  expandedMatrix=createExpandedMatrix(sourceMatrix)
+  sourcenames=colnames(expandedMatrix)
   n=nrow(sourceMatrix)
-  expandedmatrix=data.frame(row.names=1:n)  
+  finalMatrix=data.frame(row.names=1:n)  
   for (colname in colnames){
     if (colname %in% sourcenames){
-      expandedmatrix=cbind(expandedmatrix,sourceMatrix[,colname,drop=FALSE])
+      finalMatrix=cbind(finalMatrix,expandedMatrix[,colname,drop=FALSE])
     }
     else{
       zero_level=matrix(0,n,1)
       colnames(zero_level)=colname
-      expandedmatrix=cbind(expandedmatrix,zero_level)
+      finalMatrix=cbind(finalMatrix,zero_level)
     }
   }
-  expandedmatrix
+  finalMatrix
 }
 
+#Loading  train data
+train <- read.csv("train.csv")
 
-finalM=createExpandedMatrix(total)
+# Removing columns
+train.x =train[-1]
+train.x =train.x[-82]
+# Extracting training y
+train.y=log(train[,"Sale_Price"])
 
+# Create train Matrix
 trainM=createExpandedMatrix(train.x)
-testM=createExpandedMatrix(test.x)
+# Get list of columns
+ctrainM=colnames(trainM)
 
-cfinalM=colnames(finalM)
 
-trainExp=expandMatrix(trainM,cfinalM)
-testExp=expandMatrix(testM,cfinalM)
-
-start.time <- Sys.time()
-xgb.model <- xgboost(data = as.matrix(trainExp), 
-                     label = train.y, max_depth = 6,
-                     eta = 0.05, nrounds = 1000,
-                     subsample = 0.3,
+# train Model
+params <- list(
+  eta = 0.5,
+  max_depth = 6,
+  subsample = 0.8,
+  colsample_bytree = 0.5,
+  gamma=0,
+  lambda=0,
+  alpha=1
+)
+xgb.model <- xgboost(data = as.matrix(trainM), 
+                     label = train.y,
+                     params = params,
+                     nrounds=500,
                      verbose = FALSE)
-end.time <- Sys.time()
-time.taken <- end.time - start.time
-time.taken
 
-pred=exp(predict(xgb.model,as.matrix(testExp)))
+
+# Load test data
+test <- read.csv("test.csv")
+# Removing columns
+test.x=test[-1]
+
+testM=adjustMatrix(test.x,ctrainM)
+
+pred=exp(predict(xgb.model,as.matrix(testM)))
 
 pred=cbind(test["PID"],pred)
 
 names(pred)[2]="Sale_Price"
 
 write.csv(pred,"mysubmission1.txt",row.names = FALSE)
+
+
+##
